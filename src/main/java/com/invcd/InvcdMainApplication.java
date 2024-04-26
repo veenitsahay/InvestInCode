1525ed8282b2bd63324bd105103f9c26173b7669
vesahay <veenit.sahay@soprabanking.com>
2024-04-26 20:05:10 +0100

    First Commit

---
+++ src/main/java/com/invcd/InvcdMainApplication.java
@@ -0,0 +1,13 @@
+package com.invcd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvcdMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvcdMainApplication.class, args);
+	}
+
+}
