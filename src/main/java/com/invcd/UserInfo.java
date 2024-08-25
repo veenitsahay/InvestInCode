b420c0ef153576810dbbe5b913c805ac4fbf2512
vesahay <veenit.sahay@soprabanking.com>
2024-08-25 13:30:53 +0100

    Rest API and User repository created

---
+++ src/main/java/com/invcd/UserInfo.java
@@ -0,0 +1,48 @@
+package com.invcd;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "userinfo")
+public class UserInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
